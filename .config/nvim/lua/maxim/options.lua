6e378edd432ded4c50d3274997ed36744fdb29eb
Maxim Yurevich <maxim.yurewitch@gmail.com>
2023-11-03 23:50:04 +0300

    feat(nvim): add tabline keymaps

--- .config/nvim/lua/maxim/options.lua
+++ .config/nvim/lua/maxim/options.lua
@@ -20,3 +20,5 @@ vim.o.sessionoptions =
 	"blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 
 vim.cmd("au BufNewFile,BufRead PKGBUILD set filetype=PKGBUILD")
+
+require("maxim.tabline")
