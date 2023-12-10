b60d7bc1c4ac1b4e623f237fd61bd0933ff94057
Maxim Yurevich <maxim.yurewitch@gmail.com>
2023-12-10 16:00:49 +0300

    refactoring(awesomewm): change wallpaper

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -7,7 +7,7 @@ run() {
 }
 
 run nm-applet
-run feh --bg-scale /usr/share/backgrounds/linuxmint-vera/pfore_mount_baker.jpg
+run feh --bg-scale /usr/share/70fijyp9zyq11.png
 run redshift -P -O 3500
 run picom --config ~/.config/picom.conf
 run xss-lock --transfer-sleep-lock -- i3lock-fancy --nofork
