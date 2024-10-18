0a53469fff840b1144473c6b8453df9e3fb988c4
Maksim Yurevich <maxim.yurewitch@gmail.com>
2024-10-18 15:23:49 +0300

    refactor(nvim): remove `on_attach` property for `biome`

--- .config/nvim/lua/maxim/plugins/mason.lua
+++ .config/nvim/lua/maxim/plugins/mason.lua
@@ -151,14 +151,6 @@ return {
           ["biome"] = function()
             lspconfig.biome.setup({
               root_dir = lspconfig.util.root_pattern("biome.json", "biome.jsonc"),
-              on_attach = function(_, bufnr)
-                vim.api.nvim_create_autocmd("BufWritePre", {
-                  buffer = bufnr,
-                  callback = function()
-                    vim.lsp.buf.format({ bufnr = bufnr, async = false })
-                  end,
-                })
-              end,
             })
           end,
           ["volar"] = function()
